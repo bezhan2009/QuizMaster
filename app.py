1c26bfa1dee43768fec72ad91e103b53f18abe73
bezhan2009 <karimovbezan0@gmail.com>
2024-07-20 12:39:31 +0300

    add index

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
-from flask import Flask, jsonify, url_for, request, render_template, redirect, session
-import psycopg2
-from models import create_tables
+try:
+    from flask import Flask, jsonify, url_for, request, render_template, redirect, session
+    import psycopg2
+    from models import create_tables
 
-app = Flask(__name__)
-app.secret_key = 'bezhan200910203040'
+    app = Flask(__name__)
+    app.secret_key = 'bezhan200910203040'
 
-
-# Создание таблиц должно происходить при инициализации приложения, но не в глобальном пространстве имен
-def initialize_db():
-    try:
-        create_tables()
-    except psycopg2.Error as e:
-        print(f"Error creating tables: {e}")
-
-
-initialize_db()
+    def initialize_db():
+        try:
+            create_tables()
+        except psycopg2.Error as e:
+            print(f"Error creating tables: {e}")
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+    initialize_db()
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    @app.route('/', methods=['GET'])
+    def index():
+        return render_template('index.html')
+
+
+    if __name__ == '__main__':
+        app.run(debug=True)
+
+except Exception as e:
+    from err_utils import get_err
+
+    get_err(e)
